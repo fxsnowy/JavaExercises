60c2c354f4b2612294b9128028bd8f11e52de8b2
Yannick Daly <yannickdaly@gmail.com>
2013-09-03 16:55:15 -0400

    Update BMICalculator.java

--- BMICalculator.java
+++ BMICalculator.java
@@ -8,15 +8,22 @@ public class BMICalculator
 	{
 		Scanner keyboard = new Scanner(System.in);
 		double lb, in, ft, m, kg, bmi;
+		//The program will take input in feet and pounds.
 
 		System.out.print("Your height? (feet only): ");
 		ft = keyboard.nextDouble();
-
+		
 		System.out.print("Your height? (inches)");
 		in = keyboard.nextDouble();
 
 		System.out.print("Your weight in pounds: ");
 		lb = keyboard.nextDouble();
+		
+		//The following code converts feet/in and pounds into
+		//meters and kilograms respectively to calculate BMI
+		//without the user having to convert the numbers themselves.
+		// A foot is equal to 0.3048 meters and an inch is equal to 0.0254 meters
+		// A pound is equal to 0.453592 kilograms.
 
 		m = (ft * 0.3048) + (in * 0.0254);
 
